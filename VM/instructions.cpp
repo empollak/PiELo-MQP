1765167da47975648e2328f92aa77c9cfa55ca9d
fmarcantoni <fmarcantoni@wpi.edu>
2024-11-16 17:02:21 -0500

    added add, mul, div and mod

--- VM/instructions.cpp
+++ VM/instructions.cpp
@@ -41,10 +41,10 @@ namespace PiELo{
                 Variable result =  static_cast<float>(a.getIntValue()) + b.getFloatValue();
                 stack.push(result);
             } else if(a.type == INT && b.type == INT){
-                Variable result =  a.getIntValue()) + b.getIntValue();
+                Variable result =  a.getIntValue() + b.getIntValue();
                 stack.push(result);
             } else { 
-                throw InvalidTypeForOperationException("CLOSURE");
+                throw InvalidTypeForOperationException("ADD", "CLOSURE");
                 state = ERROR;
             }
         } else {
@@ -65,20 +65,105 @@ namespace PiELo{
                 throw InvalidTypeForOperationException("SUB", "NIL");
                 state = ERROR;
             } else if(a.type == FLOAT && b.type != FLOAT){
-                Variable result = a.getFloatValue() + static_cast<float>(b.getIntValue());
+                Variable result = static_cast<float>(b.getIntValue()) - a.getFloatValue();
                 stack.push(result);
             } else if(a.type != FLOAT && b.type == FLOAT){
-                Variable result =  static_cast<float>(a.getIntValue()) + b.getFloatValue();
+                Variable result =  b.getFloatValue() - static_cast<float>(a.getIntValue());
                 stack.push(result);
             } else if(a.type == INT && b.type == INT){
-                Variable result =  a.getIntValue()) + b.getIntValue();
+                Variable result = b.getIntValue() - a.getIntValue();
                 stack.push(result);
             } else { 
-                throw InvalidTypeForOperationException("CLOSURE");
+                throw InvalidTypeForOperationException("SUB", "CLOSURE");
                 state = ERROR;
             }
         } else {
-            throw ShortOnElementsOnStackException("ADD");
+            throw ShortOnElementsOnStackException("SUB");
+            state = ERROR;
+        }
+    }
+
+    void mul(){ 
+        if(stack.size() >= 2) {
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if(a.type == NAME || b.type == NAME){
+                throw InvalidTypeForOperationException("MUL", "STRING");
+                state = ERROR;
+            } else if(a.type == NIL || b.type == NIL) {
+                throw InvalidTypeForOperationException("MUL", "NIL");
+                state = ERROR;
+            } else if(a.type == FLOAT && b.type != FLOAT){
+                Variable result = static_cast<float>(b.getIntValue()) * a.getFloatValue();
+                stack.push(result);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                Variable result = b.getFloatValue() * static_cast<float>(a.getIntValue());
+                stack.push(result);
+            } else if(a.type == INT && b.type == INT){
+                Variable result = b.getIntValue() * a.getIntValue();
+                stack.push(result);
+            } else { 
+                throw InvalidTypeForOperationException("MUL", "CLOSURE");
+                state = ERROR;
+            }
+        } else {
+            throw ShortOnElementsOnStackException("MUL");
+            state = ERROR;
+        }
+    }
+
+    void div(){ // divide from the second element in the stack(b) the top of the stack(a)
+        if(stack.size() >= 2) {
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if(a.type == NAME || b.type == NAME){
+                throw InvalidTypeForOperationException("DIV", "STRING");
+                state = ERROR;
+            } else if(a.type == NIL || b.type == NIL) {
+                throw InvalidTypeForOperationException("DIV", "NIL");
+                state = ERROR;
+            } else if((a.type == INT && a.getIntValue() == 0) || (a.type == FLOAT && a.getFloatValue() == 0.0f)){
+                throw DivisionByZeroException();
+                state = ERROR;
+            }
+            else if(a.type == FLOAT && b.type != FLOAT){
+                Variable result = static_cast<float>(b.getIntValue()) / a.getFloatValue();
+                stack.push(result);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                Variable result =  b.getFloatValue() / static_cast<float>(a.getIntValue());
+                stack.push(result);
+            } else if(a.type == INT && b.type == INT){
+                Variable result = b.getIntValue() / a.getIntValue();
+                stack.push(result);
+            } else { 
+                throw InvalidTypeForOperationException("SUB", "CLOSURE");
+                state = ERROR;
+            }
+        } else {
+            throw ShortOnElementsOnStackException("SUB");
+            state = ERROR;
+        }
+    }
+
+    void mod(){
+        if(stack.size() >= 2) {
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if(a.type != INT || b.type != INT) {
+                throw InvalidTypeForOperationException("MOD", "!= than INT. Only INT type is allowed.");
+                state = ERROR;
+            } else if (a.getIntValue() == 0){
+                throw DivisionByZeroException();
+                state = ERROR;
+            } else {
+                Variable result = b.getIntValue() % a.getIntValue();
+                stack.push(result);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("MOD");
             state = ERROR;
         }
     }

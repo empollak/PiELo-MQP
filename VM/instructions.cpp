36f8b117208889bc85cd21d3e1cc1f13d35f0059
fmarcantoni <fmarcantoni@wpi.edu>
2024-11-17 00:23:24 -0500

    added eql, neql, lt, lte, gt, gte, gotta check for closures making it illegal

--- VM/instructions.cpp
+++ VM/instructions.cpp
@@ -222,6 +222,175 @@ namespace PiELo{
         }
     }
 
+    void neql(){
+        if(stack.size() >= 2){
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if((a.type == NAME && b.type != NAME) || (a.type != NAME && b.type == NAME)){
+                stack.push(1);
+            } else if(a.type == NAME && b.type == NAME){
+                stack.push(a.getNameValue() == b.getNameValue() ? 1:0);
+            } else if((a.type == NIL && b.type != NIL) || (a.type != NIL && b.type == NIL)){
+                stack.push(1);
+            } else if(a.type == NIL && b.type == NIL){
+                stack.push(0);
+            } 
+            // check for closure, comparing closures should be illegal
+            else if(a.type == FLOAT && b.type != FLOAT){
+                float conversion_value = static_cast<float>(b.getIntValue());
+                stack.push(a.getFloatValue() != conversion_value ? 1 : 0);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                float conversion_value = static_cast<float>(a.getIntValue());
+                stack.push(conversion_value != b.getFloatValue() ? 1 : 0);
+            } else if(a.type == FLOAT && b.type == FLOAT){
+                stack.push(a.getFloatValue() != b.getFloatValue() ? 1 : 0);
+            } else {
+                stack.push(a.getIntValue() != b.getIntValue() ? 1 : 0);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("NEQL");
+            state = ERROR;
+        }
+    }
+
+    void gt(){
+        if(stack.size() >= 2){
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if((a.type == NAME && b.type != NAME) || (a.type != NAME && b.type == NAME)){
+                throw InvalidTypeForOperationException("GT", "NAME");
+                state = ERROR;
+            } else if(a.type == NAME && b.type == NAME){
+                stack.push(a.getNameValue() > b.getNameValue() ? 1:0);
+            } else if((a.type == NIL && b.type != NIL) || (a.type != NIL && b.type == NIL)){
+                throw InvalidTypeForOperationException("GT", "NIL");
+                state = ERROR;
+            } else if(a.type == NIL && b.type == NIL){
+                stack.push(0);
+            } 
+            // check for closure, comparing closures should be illegal
+            else if(a.type == FLOAT && b.type != FLOAT){
+                float conversion_value = static_cast<float>(b.getIntValue());
+                stack.push(a.getFloatValue() > conversion_value ? 1 : 0);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                float conversion_value = static_cast<float>(a.getIntValue());
+                stack.push(conversion_value > b.getFloatValue() ? 1 : 0);
+            } else if(a.type == FLOAT && b.type == FLOAT){
+                stack.push(a.getFloatValue() > b.getFloatValue() ? 1 : 0);
+            } else {
+                stack.push(a.getIntValue() > b.getIntValue() ? 1 : 0);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("GT");
+            state = ERROR;
+        }
+    }
+
+    void gte(){
+        if(stack.size() >= 2){
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if((a.type == NAME && b.type != NAME) || (a.type != NAME && b.type == NAME)){
+                throw InvalidTypeForOperationException("GTE", "NAME");
+                state = ERROR;
+            } else if(a.type == NAME && b.type == NAME){
+                stack.push(a.getNameValue() >= b.getNameValue() ? 1:0);
+            } else if((a.type == NIL && b.type != NIL) || (a.type != NIL && b.type == NIL)){
+                throw InvalidTypeForOperationException("GTE", "NIL");
+                state = ERROR;
+            } else if(a.type == NIL && b.type == NIL){
+                stack.push(1);
+            } 
+            // check for closure, comparing closures should be illegal
+            else if(a.type == FLOAT && b.type != FLOAT){
+                float conversion_value = static_cast<float>(b.getIntValue());
+                stack.push(a.getFloatValue() >= conversion_value ? 1 : 0);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                float conversion_value = static_cast<float>(a.getIntValue());
+                stack.push(conversion_value >= b.getFloatValue() ? 1 : 0);
+            } else if(a.type == FLOAT && b.type == FLOAT){
+                stack.push(a.getFloatValue() >= b.getFloatValue() ? 1 : 0);
+            } else {
+                stack.push(a.getIntValue() >= b.getIntValue() ? 1 : 0);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("GTE");
+            state = ERROR;
+        }
+    }
+
+
+    void lt(){
+        if(stack.size() >= 2){
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if((a.type == NAME && b.type != NAME) || (a.type != NAME && b.type == NAME)){
+                throw InvalidTypeForOperationException("LT", "NAME");
+                state = ERROR;
+            } else if(a.type == NAME && b.type == NAME){
+                stack.push(a.getNameValue() < b.getNameValue() ? 1:0);
+            } else if((a.type == NIL && b.type != NIL) || (a.type != NIL && b.type == NIL)){
+                throw InvalidTypeForOperationException("LT", "NIL");
+                state = ERROR;
+            } else if(a.type == NIL && b.type == NIL){
+                stack.push(0);
+            } 
+            // check for closure, comparing closures should be illegal
+            else if(a.type == FLOAT && b.type != FLOAT){
+                float conversion_value = static_cast<float>(b.getIntValue());
+                stack.push(a.getFloatValue() < conversion_value ? 1 : 0);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                float conversion_value = static_cast<float>(a.getIntValue());
+                stack.push(conversion_value < b.getFloatValue() ? 1 : 0);
+            } else if(a.type == FLOAT && b.type == FLOAT){
+                stack.push(a.getFloatValue() < b.getFloatValue() ? 1 : 0);
+            } else {
+                stack.push(a.getIntValue() < b.getIntValue() ? 1 : 0);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("LT");
+            state = ERROR;
+        }
+    }
+
+    void lte(){
+        if(stack.size() >= 2){
+            Variable a = stack.top(); stack.pop();
+            Variable b = stack.top(); stack.pop();
+
+            if((a.type == NAME && b.type != NAME) || (a.type != NAME && b.type == NAME)){
+                throw InvalidTypeForOperationException("LTE", "NAME");
+                state = ERROR;
+            } else if(a.type == NAME && b.type == NAME){
+                stack.push(a.getNameValue() <= b.getNameValue() ? 1:0);
+            } else if((a.type == NIL && b.type != NIL) || (a.type != NIL && b.type == NIL)){
+                throw InvalidTypeForOperationException("LTE", "NIL");
+                state = ERROR;
+            } else if(a.type == NIL && b.type == NIL){
+                stack.push(1);
+            } 
+            // check for closure, comparing closures should be illegal
+            else if(a.type == FLOAT && b.type != FLOAT){
+                float conversion_value = static_cast<float>(b.getIntValue());
+                stack.push(a.getFloatValue() <= conversion_value ? 1 : 0);
+            } else if(a.type != FLOAT && b.type == FLOAT){
+                float conversion_value = static_cast<float>(a.getIntValue());
+                stack.push(conversion_value <= b.getFloatValue() ? 1 : 0);
+            } else if(a.type == FLOAT && b.type == FLOAT){
+                stack.push(a.getFloatValue() <= b.getFloatValue() ? 1 : 0);
+            } else {
+                stack.push(a.getIntValue() <= b.getIntValue() ? 1 : 0);
+            }
+        } else {
+            throw ShortOnElementsOnStackException("GTE");
+            state = ERROR;
+        }
+    }
+
     void pop() {
         stack.pop();
     }

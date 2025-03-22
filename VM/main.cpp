0895fa225f5c41f990e715bdd098e1cab1cf1695
Emma Pollak <123012831+empollak@users.noreply.github.com>
2025-03-22 12:21:31 -0400

    Add random sleep function

--- VM/main.cpp
+++ VM/main.cpp
@@ -1,5 +1,9 @@
 #include "vm.h"
 #include "robotFunctions.h"
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <sys/time.h>
 
 int doNothingButTalkAboutIt() {
     std::cout << "doing nothing!!" << std::endl;
@@ -18,14 +22,28 @@ int printRobotPos() {
     return 0;
 }
 
+int randomSleep() {
+    int maxSleepUs = 3000000;
+    int sleepTime = rand() % maxSleepUs;
+    std::cout << "Sleeping for " << sleepTime / 1000.0 << "ms " << std::endl;
+    usleep(sleepTime);
+    return 0;
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         printf("Please provide a filename\n");
         exit(-1);
     }
+    // Get a random seed based on the microsecond-precision time this program is running
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    srand(time.tv_sec * time.tv_usec);
+
     PiELo::registerFunction("do_nothing", &doNothingButTalkAboutIt);
     PiELo::registerFunction("go_forward", &goForward);
     PiELo::registerFunction("print_robot_pos", &printRobotPos);
+    PiELo::registerFunction("random_sleep", &randomSleep);
     PiELo::load(argv[1]);
     while(PiELo::step() == PiELo::VMState::READY);
     printf("Done!\n");
